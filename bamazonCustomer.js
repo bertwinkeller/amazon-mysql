ff55a2748393f8c9a9ae0196bf93a927d4b4dece
Bert Keller <bertwinkeller@gmail.com>
2019-11-07 11:21:57 -0800

    console logging producs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,20 @@
-var mysql = require('mysql2');
 
+// require node packages
+var mysql = require('mysql2');
+var inquirer = require('inquirer');
+
+
+
+inquirer
+  .prompt([
+      {
+
+      }
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
 var db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -9,8 +24,16 @@ var db = mysql.createConnection({
 
 db.connect(function(err) {
     if (err) throw err;
-    db.query("SELECT * FROM products", function (err, result, fields) {
+    db.query("SELECT product_name,department_name,price FROM products", function (err, result, fields) {
       if (err) throw err;
-      console.log(result);
+      console.log(`------Welcome to Bamazon-------`)
+      console.log('------Available Products-------')
+      for(let i=0;i<result.length;i++){
+      console.log(`-------------------------------`)
+      console.log(`Item: ${result[i].product_name}`)
+      console.log(`Department: ${result[i].department_name}`)
+      console.log(`Price: ${result[i].price}`)
+      console.log(`-------------------------------`)
+      }
     });
   });
